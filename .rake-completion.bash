59fec83312c628be46c5fd8fca92b4432186d313
Eric Cohen <eirc.eirc@gmail.com>
2011-12-21 13:35:38 +0200

    Better completion for rake.
    Inspired by http://railscasts.com/episodes/309. Thanks @ryanb.

--- .rake-completion.bash
+++ .rake-completion.bash
@@ -1,9 +1,14 @@
 # bash-completion for rake tasks
-# rake -s -T 2>/dev/null | awk '{{print $2}}' > .rake
 
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
-_rakecomplete() {
-  COMPREPLY=($(compgen -W "`if [ -f .rake ]; then cat .rake; fi`" -- ${COMP_WORDS[COMP_CWORD]}))
+_rake() {
+  if [ -f Rakefile ]; then
+    if [ ! -f ._rake~ ]; then
+      rake --tasks 2>/dev/null | cut -d ' ' -f 2 > ._rake~
+    fi
+    COMPREPLY=($(compgen -W "`cat ._rake~`" -- ${COMP_WORDS[COMP_CWORD]}))
+  fi
   return 0
 }
-complete -o default -o nospace -F _rakecomplete rake
+
+complete -o default -o nospace -F _rake rake
